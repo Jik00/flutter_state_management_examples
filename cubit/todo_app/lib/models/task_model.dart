0a3797eed4df0890616a8c1d6aea5285d1d308d7
Jiko <youstinahabib422@gmail.com>
2025-09-08 17:25:12 +0300

    Cubit todoApp / add Equatable + task mode

---
+++ cubit/todo_app/lib/models/task_model.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+
+class TaskModel extends Equatable {
+  final String id;
+  final String title;
+  final bool isCompleted;
+
+  const TaskModel({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  // required method for the equatable package
+  TaskModel copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+  }) {
+    return TaskModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, title, isCompleted];
+
+}
