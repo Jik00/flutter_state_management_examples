72873dae4a8f8857cfaabba9a6fe421b79910b05
Jiko <youstinahabib422@gmail.com>
2025-09-08 19:35:40 +0300

    cubit/todoApp : create taskCubitState

---
+++ cubit/todo_app/lib/controllers/task_cubit_state.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:todo_app/models/task_model.dart';
+
+sealed class TaskCubitState extends Equatable {
+  final List<TaskModel> tasksList;
+
+  const TaskCubitState({required this.tasksList});
+
+  @override
+  List<Object?> get props => [tasksList];
+}
+
+final class TaskInitial extends TaskCubitState {
+  TaskInitial() : super(tasksList: []);
+}
+
+final class TaskUpdated extends TaskCubitState {
+  const TaskUpdated({required super.tasksList});
+}
+
